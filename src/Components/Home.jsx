b9f75f625b4563c0ac9dab78993d54e9a70e1596
Anamika Jha <ajha5169@gmail.com>
2023-10-12 23:56:25 +0545

    initial commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Navbar from "./Navbar";
+
+const Home = () => {
+    return (
+    <div className="home-container">
+      <Navbar/>
+    </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from "../Assets/Logo.svg";
+import {BsCart2} from "react-icons/bs";
+import { HiOutlineBars3} from "react-icons/hi2";
+import{}
+
+const Navbar = () => {
+  return <div>Navbar</div>
+ 
+}
+
+export default Navbar;
