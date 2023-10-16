246bc8df21f5aa6cf43ed593d74746cc38f71063
Anamika Jha <ajha5169@gmail.com>
2023-10-16 23:56:17 +0530

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,11 +2,45 @@ import React from 'react';
 import Logo from "../Assets/Logo.svg";
 import {BsCart2} from "react-icons/bs";
 import { HiOutlineBars3} from "react-icons/hi2";
-import{}
+import{
+  Box,
+  Drawer,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home"; 
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 
 const Navbar = () => {
-  return <div>Navbar</div>
- 
-}
+
+  const [openMenu, setOpenMenu] =  useState(false)
+  const menuOptions = [
+  
+    {
+      text: "Home",
+      icon: <HomeIcon/>
+    },
+    {
+      text: "About",
+      icon: <InfoIcon/>
+    },
+    {
+      text: "Testimonials",
+      icon: <CommentRoundedIcon/>
+    },
+    {
+      text: "Cart",
+      icon: <ShoppingCartRoundedIcon/>
+    },
+  ];
+
+  return <div></div>
+  
+};
 
 export default Navbar;
