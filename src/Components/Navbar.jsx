78e609dc2c07ec0ff9a77ffa9739b74fac743673
Anamika Jha <89740849+anamiikajha@users.noreply.github.com>
2023-10-20 00:00:04 +0530

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
   <div className="navbar-menu-container">
     <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
   </div>
-  <Drawer open = {openMenu} onClose {() => setOpenMenu(false)}
+  <Drawer open = {openMenu} onClose ={() => setOpenMenu(false)}
   anchor =  "right">
       <Box 
          sx = {{ width: 250}}
