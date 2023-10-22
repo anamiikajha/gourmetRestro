ed161982797b2487a43a63613937d4fb4c984cec
Anamika Jha <89740849+anamiikajha@users.noreply.github.com>
2023-10-22 23:56:40 +0530

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Navbar from "./Navbar";
-import BannerBackground from "../Assets/home-banner-background.png"
+import BannerBackground from "../Assets/home-banner-image.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import { FiArrowRight } from "react-icons/fi";
 
 const Home = () => {
     return (
